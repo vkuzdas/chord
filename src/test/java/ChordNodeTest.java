774596115679f3816dc1beefc8c1c9394dee5380
vojta <kuzdavoj@fel.cvut.cz>
2024-03-07 10:48:53 +0100

    Revert to "getAddress()"
    This reverts commit 36819e9cc6e648421fa76ab55630e789578605e8.
    
    Revert "mvn test put back, awaitility tests ignored"
    
    This reverts commit 912aed63b79cd7db864e39508fa2450c85cba36d.
    
    Revert "mvn test *ACTUALLY* put back"
    
    This reverts commit 8525fb9c5ab6de71ff39610fae8702370684fe7f.
    
    Revert "tests are probably unstable"
    
    This reverts commit b929953c3feb24d3c7335ae35493b8a19e20e581.
    
    Revert "only green tests"
    
    This reverts commit e02306e1a722854afa5557fec683ca5d620b941b.
    
    Revert "try to completely remove awaitility"
    
    This reverts commit 7caf1c45ed83e3b946d2693521d99d3073ed753f.

--- src/test/java/ChordNodeTest.java
+++ src/test/java/ChordNodeTest.java
@@ -1,10 +1,9 @@
 import chord.ChordNode;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-//import static java.util.concurrent.TimeUnit.SECONDS;
-//import static org.awaitility.Awaitility.await;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChordNodeTest {
@@ -62,133 +61,129 @@ class ChordNodeTest {
         node2.stopServer();
     }
 
-//    @Test
-//    @Disabled
-//    void threeJoinOnBootstrap() throws Exception {
-//        ChordNode bootstrap = new ChordNode("localhost", 8980);
-//        bootstrap.startServer();
-//
-//        ChordNode node2 = new ChordNode("localhost", 8981);
-//        node2.startServer();
-//        node2.join(bootstrap);
-//
-//        ChordNode node3 = new ChordNode("localhost", 8982);
-//        node3.startServer();
-//        node3.join(bootstrap);
-//
-//        assertEquals(bootstrap.getNodeReference().id, 10);
-//        assertEquals(node2.getNodeReference().id, 0);
-//        assertEquals(node3.getNodeReference().id, 2);
-//
-//        int timeoutSeconds = 2;
-//
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getPredecessor().id == 2);
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getSuccessor().id == 0);
-//
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getPredecessor().id == 10);
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getSuccessor().id == 2);
-//
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getPredecessor().id == 0);
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getSuccessor().id == 10);
-//
-//        bootstrap.stopServer();
-//        node2.stopServer();
-//        node3.stopServer();
-//    }
-//
-//    @Test
-//    @Disabled
-//    void chainedJoin() throws Exception {
-//        ChordNode bootstrap = new ChordNode("localhost", 8980);
-//        bootstrap.startServer();
-//
-//        ChordNode node2 = new ChordNode("localhost", 8981);
-//        node2.startServer();
-//        node2.join(bootstrap);
-//
-//        ChordNode node3 = new ChordNode("localhost", 8982);
-//        node3.startServer();
-//        node3.join(node2);
-//
-//        assertEquals(bootstrap.getNodeReference().id, 10);
-//        assertEquals(node2.getNodeReference().id, 0);
-//        assertEquals(node3.getNodeReference().id, 2);
-//
-//        int timeoutSeconds = 2;
-//
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getPredecessor().id == 2);
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getSuccessor().id == 0);
-//
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getPredecessor().id == 10);
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getSuccessor().id == 2);
-//
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getPredecessor().id == 0);
-//        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getSuccessor().id == 10);
-//
-//        bootstrap.stopServer();
-//        node2.stopServer();
-//        node3.stopServer();
-//    }
-//
-//    @Test
-//    @Disabled
-//    void leaveFromBootstrap() throws Exception {
-//        ChordNode bootstrap = new ChordNode("localhost", 8980);
-//        bootstrap.startServer();
-//
-//        ChordNode node2 = new ChordNode("localhost", 8981);
-//        node2.startServer();
-//        node2.join(bootstrap);
-//
-//        ChordNode node3 = new ChordNode("localhost", 8982);
-//        node3.startServer();
-//        node3.join(bootstrap);
-//
-//        Thread.sleep(FIX_FINGER_TIMEOUT); // let the network stabilize
-//        node2.leave();
-//        node2.stopServer();
-//
-//        // assert that there are no references to the node that left
-//        // FIXME: this does not validate FingerTable eradication since it just so happens to not be there
-//        assertNotEquals(bootstrap.getPredecessor(), node2.getNodeReference());
-//        assertFalse(bootstrap.containedInFingerTable(node2.getNodeReference()));
-//
-//        assertNotEquals(node3.getPredecessor(), node2.getNodeReference());
-//        assertFalse(node3.containedInFingerTable(node2.getNodeReference()));
-//
-//        bootstrap.stopServer();
-//        node3.stopServer();
-//    }
-//
-//    @Test
-//    @Disabled
-//    void chainedLeave() throws Exception {
-//        ChordNode bootstrap = new ChordNode("localhost", 8980);
-//        bootstrap.startServer();
-//
-//        ChordNode node2 = new ChordNode("localhost", 8981);
-//        node2.startServer();
-//        node2.join(bootstrap);
-//
-//        ChordNode node3 = new ChordNode("localhost", 8982);
-//        node3.startServer();
-//        node3.join(node2);
-//
-//        Thread.sleep(FIX_FINGER_TIMEOUT); // let the network stabilize
-//        node2.stopServer();
-//        node2.leave();
-//
-//
-//        // FIXME: this does not validate FingerTable eradication since it just so happens to not be there
-//        assertNotEquals(bootstrap.getPredecessor(), node2.getNodeReference());
-//        assertFalse(bootstrap.containedInFingerTable(node2.getNodeReference()));
-//
-//        assertNotEquals(node3.getPredecessor(), node2.getNodeReference());
-//        assertFalse(node3.containedInFingerTable(node2.getNodeReference()));
-//
-//
-//        bootstrap.stopServer();
-//        node3.stopServer();
-//    }
+    @Test
+    void threeJoinOnBootstrap() throws Exception {
+        ChordNode bootstrap = new ChordNode("localhost", 8980);
+        bootstrap.startServer();
+
+        ChordNode node2 = new ChordNode("localhost", 8981);
+        node2.startServer();
+        node2.join(bootstrap);
+
+        ChordNode node3 = new ChordNode("localhost", 8982);
+        node3.startServer();
+        node3.join(bootstrap);
+
+        assertEquals(bootstrap.getNodeReference().id, 10);
+        assertEquals(node2.getNodeReference().id, 0);
+        assertEquals(node3.getNodeReference().id, 2);
+
+        int timeoutSeconds = 2;
+
+        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getPredecessor().id == 2);
+        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getSuccessor().id == 0);
+
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getPredecessor().id == 10);
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getSuccessor().id == 2);
+
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getPredecessor().id == 0);
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getSuccessor().id == 10);
+
+        bootstrap.stopServer();
+        node2.stopServer();
+        node3.stopServer();
+    }
+
+    @Test
+    void chainedJoin() throws Exception {
+        ChordNode bootstrap = new ChordNode("localhost", 8980);
+        bootstrap.startServer();
+
+        ChordNode node2 = new ChordNode("localhost", 8981);
+        node2.startServer();
+        node2.join(bootstrap);
+
+        ChordNode node3 = new ChordNode("localhost", 8982);
+        node3.startServer();
+        node3.join(node2);
+
+        assertEquals(bootstrap.getNodeReference().id, 10);
+        assertEquals(node2.getNodeReference().id, 0);
+        assertEquals(node3.getNodeReference().id, 2);
+
+        int timeoutSeconds = 2;
+
+        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getPredecessor().id == 2);
+        await().atMost(timeoutSeconds, SECONDS).until(() -> bootstrap.getSuccessor().id == 0);
+
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getPredecessor().id == 10);
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node2.getSuccessor().id == 2);
+
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getPredecessor().id == 0);
+        await().atMost(timeoutSeconds, SECONDS).until(() -> node3.getSuccessor().id == 10);
+
+        bootstrap.stopServer();
+        node2.stopServer();
+        node3.stopServer();
+    }
+
+    @Test
+    void leaveFromBootstrap() throws Exception {
+        ChordNode bootstrap = new ChordNode("localhost", 8980);
+        bootstrap.startServer();
+
+        ChordNode node2 = new ChordNode("localhost", 8981);
+        node2.startServer();
+        node2.join(bootstrap);
+
+        ChordNode node3 = new ChordNode("localhost", 8982);
+        node3.startServer();
+        node3.join(bootstrap);
+
+        Thread.sleep(FIX_FINGER_TIMEOUT); // let the network stabilize
+        node2.leave();
+        node2.stopServer();
+
+        // assert that there are no references to the node that left
+        // FIXME: this does not validate FingerTable eradication since it just so happens to not be there
+        assertNotEquals(bootstrap.getPredecessor(), node2.getNodeReference());
+        assertFalse(bootstrap.containedInFingerTable(node2.getNodeReference()));
+
+        assertNotEquals(node3.getPredecessor(), node2.getNodeReference());
+        assertFalse(node3.containedInFingerTable(node2.getNodeReference()));
+
+        bootstrap.stopServer();
+        node3.stopServer();
+    }
+
+    @Test
+    void chainedLeave() throws Exception {
+        ChordNode bootstrap = new ChordNode("localhost", 8980);
+        bootstrap.startServer();
+
+        ChordNode node2 = new ChordNode("localhost", 8981);
+        node2.startServer();
+        node2.join(bootstrap);
+
+        ChordNode node3 = new ChordNode("localhost", 8982);
+        node3.startServer();
+        node3.join(node2);
+
+        Thread.sleep(FIX_FINGER_TIMEOUT); // let the network stabilize
+        node2.stopServer();
+        node2.leave();
+
+
+        // FIXME: this does not validate FingerTable eradication since it just so happens to not be there
+        assertNotEquals(bootstrap.getPredecessor(), node2.getNodeReference());
+        assertFalse(bootstrap.containedInFingerTable(node2.getNodeReference()));
+
+        assertNotEquals(node3.getPredecessor(), node2.getNodeReference());
+        assertFalse(node3.containedInFingerTable(node2.getNodeReference()));
+
+
+        bootstrap.stopServer();
+        node3.stopServer();
+    }
 
 }
